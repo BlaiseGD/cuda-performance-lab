54b57ef39332462c3000b1798f6a421d2098485a
Blaise <blaisequay@gmail.com>
2026-05-20 16:41:35 -0500

    finished initial warp and fused layernorm functions

--- LayerNorm/kernel.cu
+++ LayerNorm/kernel.cu
@@ -3,10 +3,12 @@
 #include "device_launch_parameters.h"
 #include "cuda_profiler_api.h"
 #define BLOCK_SIZE 256
+#define WARP_SIZE 32
 
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <vector>
 using namespace std;
 
 
@@ -135,13 +137,131 @@ __global__ void block_layernorm_gpu(float* inputArr, float* outputArr, int token
 
 	if(token < tokenNum && hiddenThread < tokenVecLen) outputArr[token * tokenVecLen + hiddenThread] = (xi - mean[0]) / sqrtf(var[0] + epsilon);
 }
-	
+//warp reduce the mean and var and then normalize to outputArr
 __global__ void warp_layernorm_gpu(float* inputArr, float* outputArr, int tokenNum, int tokenVecLen) {
+	const int WARP_NUM = BLOCK_SIZE / WARP_SIZE;
+	//declaring shared warpSum
+	__shared__ float s_sum[WARP_NUM];
+	int token = blockIdx.x;
+	int hiddenThread = threadIdx.x;
+	float xi = (hiddenThread < tokenVecLen) ? inputArr[token * tokenVecLen + hiddenThread] : 0.0f;
+	//making sure hidden elements of same token are what's being summed
+	float sum = xi;
+	float mean = 0.0f;
+	float var = 0.0f;
 	float epsilon = 1e-5f;
+	//gets what warp we're in
+	int warpID = hiddenThread / WARP_SIZE;
+	//gets position of thread in warp
+	int laneID = hiddenThread % WARP_SIZE;
+
+	//MEAN REDUCTION
+	//get sum for each warp
+	#pragma unroll
+	for(int i = WARP_SIZE >> 1; i > 0; i >>= 1) {
+		sum += __shfl_down_sync(0xffffffff, sum, i);
+	}
+	if (laneID == 0) s_sum[warpID] = sum;
+	__syncthreads();
+	//sum for each warp acquired, reduce across the WarpSums stored in s_sum
+	if (warpID == 0) {
+
+		if (laneID < WARP_NUM) sum = s_sum[laneID];
+		else sum = 0.0f;
+			#pragma unroll
+			for (int i = WARP_NUM >> 1; i > 0; i >>= 1) {
+				sum += __shfl_down_sync(0xffffffff, sum, i);
+			}
+			if(laneID == 0) s_sum[0] = sum;
+	}
+	__syncthreads();
+	mean = s_sum[0] / (float)tokenVecLen;
+
+	//VAR REDUCTION
+	float diff = xi - mean;
+	var = diff * diff;
+	#pragma unroll
+	for (int i = WARP_SIZE >> 1; i > 0; i >>= 1) {
+		 var += __shfl_down_sync(0xffffffff, var, i);
+	}
+	if (laneID == 0) s_sum[warpID] = var;
+	__syncthreads();
+	//sum for each warp acquired, reduce across the WarpSums stored in s_sum
+	//only lanes 0 - (WARP_NUM-1) are active in this
+	if (warpID == 0) {
+
+		if (laneID < WARP_NUM) var = s_sum[laneID];
+		else var = 0.0f;
+		#pragma unroll
+		for (int i = WARP_NUM >> 1; i > 0; i >>= 1) {
+			var += __shfl_down_sync(0xffffffff, var, i);
+		}
+		if(laneID == 0) s_sum[0] = var;
+	}
+	__syncthreads();
+	var = s_sum[0] / (float)tokenVecLen;
+
+
+	if (token < tokenNum && hiddenThread < tokenVecLen) outputArr[token * tokenVecLen + hiddenThread] = (xi - mean) / sqrtf(var + epsilon);
 }
 
 __global__ void fused_layernorm_gpu(float* inputArr, float* outputArr, int tokenNum, int tokenVecLen) {
+	const int WARP_NUM = BLOCK_SIZE / WARP_SIZE;
+	//declaring shared warpSum
+	__shared__ float sdata[WARP_NUM];
+	int token = blockIdx.x;
+	int hiddenThread = threadIdx.x;
+	float xi = (hiddenThread < tokenVecLen) ? inputArr[token * tokenVecLen + hiddenThread] : 0.0f;
+	//making sure hidden elements of same token are what's being summed
+	float sum = xi;
+	float mean = 0.0f;
+	float var = 0.0f;
 	float epsilon = 1e-5f;
+	//gets what warp we're in
+	int warpID = hiddenThread / WARP_SIZE;
+	//gets position of thread in warp
+	int laneID = hiddenThread % WARP_SIZE;
+
+	//MEAN REDUCTION
+	//get sum for each warp
+	#pragma unroll
+	for (int i = WARP_SIZE >> 1; i > 0; i >>= 1) {
+		sum += __shfl_down_sync(0xffffffff, sum, i);
+	}
+	//sdata has each reduced partial sum from each warp now
+	if (laneID == 0) sdata[warpID] = sum;
+	__syncthreads();
+
+	//sum for each warp acquired, reduce across the WarpSums stored in s_sum
+	//SECOND REDUCTION PASS
+	
+	#pragma unroll
+	for (int i = WARP_NUM >> 1; i > 0; i >>= 1) {
+		if (hiddenThread < i) sdata[hiddenThread] += sdata[hiddenThread + i];
+		__syncthreads();
+	}
+	__syncthreads();
+	mean = sdata[0] / (float)tokenVecLen;
+	
+	//VAR REDUCTION
+	float diff = xi - mean;
+	var = diff * diff;
+	#pragma unroll
+	for (int i = WARP_SIZE >> 1; i > 0; i >>= 1) {
+		var += __shfl_down_sync(0xffffffff, var, i);
+	}
+	//sdata has each reduced partial variance from each warp now
+	if (laneID == 0) sdata[warpID] = var;
+	__syncthreads();
+	//SECOND REDUCTION PASS
+	#pragma unroll
+	for (int i = WARP_NUM >> 1; i > 0; i >>= 1) {
+		if(hiddenThread < i) sdata[hiddenThread] += sdata[hiddenThread + i];
+		__syncthreads();
+	}
+	__syncthreads();
+	var = sdata[0] / (float)tokenVecLen;
+	if (token < tokenNum && hiddenThread < tokenVecLen) outputArr[token * tokenVecLen + hiddenThread] = (xi - mean) / sqrtf(var + epsilon);
 }
 
 
@@ -149,71 +269,151 @@ __global__ void fused_layernorm_gpu(float* inputArr, float* outputArr, int token
 int main() {
 	//small for testing purposes
 	//for profiling hiddensize = 256, 512, etc. SequenceLen = 1024, 2048, etc
-	const int sequenceLen = 4;
-	const int hiddenSize = 16;
+	const int sequenceLen = 1024;
+	const int hiddenSize = BLOCK_SIZE;
 	const int totalElements = sequenceLen * hiddenSize;
+	//float totalNaive = 0.0f;
+	//float totalBlock = 0.0f;
 
-	float h_input[totalElements];
-	float h_output_cpu[totalElements] = { 0 };
-	float h_output_naive[totalElements] = { 0 };
-	float h_output_block[totalElements] = { 0 };
-	float* d_input, float* d_output_naive, float* d_output_block;
+	vector<float> h_input(totalElements);
+	vector<float> h_output_cpu(totalElements, 0.0f);
+	vector<float> h_output_naive(totalElements, 0.0f);
+	vector<float> h_output_block(totalElements, 0.0f);
+	vector<float> h_output_warp(totalElements, 0.0f);
+	vector<float> h_output_fused(totalElements, 0.0f);
+
+	float* d_input, *d_output_naive, *d_output_block, *d_output_warp, *d_output_fused;
 
 	//filling input with random floats from 0-9
 	for (int i = 0; i < sequenceLen * hiddenSize; i++) {
-		h_input[i] = (float)(rand() % 10);
+		h_input[i] = ((float)rand() / RAND_MAX) * 1000.0f - 500.0f;
 	}
 
-	dim3 grid(((sequenceLen * hiddenSize) +  BLOCK_SIZE - 1) / BLOCK_SIZE);
 	dim3 block(BLOCK_SIZE);
 
 	//allocate, copy, and set memory for device variables
 	cudaMalloc(&d_input, totalElements * sizeof(float));
 	cudaMalloc(&d_output_naive, totalElements * sizeof(float));
 	cudaMalloc(&d_output_block, totalElements * sizeof(float));
-	cudaMemcpy(d_input, h_input, totalElements * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMalloc(&d_output_warp, totalElements * sizeof(float));
+	cudaMalloc(&d_output_fused, totalElements * sizeof(float));
+	cudaMemcpy(d_input, h_input.data(), totalElements * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemset(d_output_naive, 0, totalElements * sizeof(float));
 	cudaMemset(d_output_block, 0, totalElements * sizeof(float));
-	
-	auto start = chrono::steady_clock::now();
+	cudaMemset(d_output_warp, 0, totalElements * sizeof(float));
+	cudaMemset(d_output_fused, 0, totalElements * sizeof(float));
+
+	layernorm_cpu(h_input.data(), h_output_cpu.data(), sequenceLen, hiddenSize);
+	//warmup
+	for (int i = 0; i < 10; i++) {
+		naive_layernorm_gpu << <sequenceLen, block >> > (d_input, d_output_naive, sequenceLen, hiddenSize);
+		block_layernorm_gpu << <sequenceLen, hiddenSize, 2 * hiddenSize * sizeof(float) >> > (d_input, d_output_block, sequenceLen, hiddenSize);
+		warp_layernorm_gpu << <sequenceLen, hiddenSize >> > (d_input, d_output_warp, sequenceLen, hiddenSize);
+		fused_layernorm_gpu << <sequenceLen, hiddenSize >> > (d_input, d_output_fused, sequenceLen, hiddenSize);
+
+	}
+	cudaDeviceSynchronize();
+
+	for (int i = 0; i < 20; i++) {
+		naive_layernorm_gpu << <sequenceLen, block >> > (d_input, d_output_naive, sequenceLen, hiddenSize);
+	}
+	cudaDeviceSynchronize();
+	for (int i = 0; i < 20; i++) {
+		block_layernorm_gpu << <sequenceLen, hiddenSize, 2 * hiddenSize * sizeof(float) >> > (d_input, d_output_block, sequenceLen, hiddenSize);
+	}
+	cudaDeviceSynchronize();
+	for (int i = 0; i < 20; i++) {
+		warp_layernorm_gpu << <sequenceLen, hiddenSize>> > (d_input, d_output_warp, sequenceLen, hiddenSize);
+	}
+	cudaDeviceSynchronize();
+	for (int i = 0; i < 20; i++) {
+		fused_layernorm_gpu << <sequenceLen, hiddenSize >> > (d_input, d_output_warp, sequenceLen, hiddenSize);
+	}
+	cudaDeviceSynchronize();
+
+	/*
+	TIMING CODE
+	auto startCPU = chrono::steady_clock::now();
 
 	//calling function and kernels
-	layernorm_cpu(h_input, h_output_cpu, sequenceLen, hiddenSize);
-	auto end = chrono::steady_clock::now();
-	chrono::duration<double> cpuTime = end - start;
-	//1 thread per token
-	naive_layernorm_gpu << <sequenceLen, block >> > (d_input, d_output_naive, sequenceLen, hiddenSize);
+	layernorm_cpu(h_input.data(), h_output_cpu.data(), sequenceLen, hiddenSize);
+	auto stopCPU = chrono::steady_clock::now();
+	chrono::duration<double> cpuTime = stopCPU - startCPU;
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	//warmup
+	cudaDeviceSynchronize();
+	for (int i = 0; i < 10; i++) naive_layernorm_gpu << <sequenceLen, block >> > (d_input, d_output_naive, sequenceLen, hiddenSize);
+	cudaDeviceSynchronize();
+	//avg time
+	for (int i = 0; i < 100; i++) {
+
+		cudaEventRecord(start);
+		//1 thread per token
+		naive_layernorm_gpu << <sequenceLen, block >> > (d_input, d_output_naive, sequenceLen, hiddenSize);
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float ms;
+		cudaEventElapsedTime(&ms, start, stop);
+		totalNaive += ms;
+	}
+	cudaDeviceSynchronize();
 	//1 block per token, 1 thread per hidden dimension element
-	block_layernorm_gpu << <sequenceLen, hiddenSize, 2*hiddenSize*sizeof(float) >> > (d_input, d_output_block, sequenceLen, hiddenSize);
+	//warmup
+	for (int i = 0; i < 10; i++) block_layernorm_gpu << <sequenceLen, hiddenSize, 2 * hiddenSize * sizeof(float) >> > (d_input, d_output_block, sequenceLen, hiddenSize);
+	cudaDeviceSynchronize();
+	//avg time
+	for (int i = 0; i < 100; i++) {
+
+		cudaEventRecord(start);
+		//1 thread per token
+		block_layernorm_gpu << <sequenceLen, hiddenSize, 2 * hiddenSize * sizeof(float) >> > (d_input, d_output_block, sequenceLen, hiddenSize);
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float ms;
+		cudaEventElapsedTime(&ms, start, stop);
+		totalBlock += ms;
+	}
+	std::cout << "CPU: " << cpuTime.count() << '\n';
+	std::cout << "Naive avg: " << totalNaive / 100 << " ms\n";
+	std::cout << "Block avg: " << totalBlock / 100 << " ms\n";
+	*/
 
 	//copy back to host
-	cudaMemcpy(h_output_naive, d_output_naive, totalElements * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_output_block, d_output_naive, totalElements * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_output_naive.data(), d_output_naive, totalElements * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_output_block.data(), d_output_block, totalElements * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_output_warp.data(), d_output_warp, totalElements * sizeof(float), cudaMemcpyDeviceToHost);
 
+	/*
 	//COMPUTE ERROR & CHECKING OUTPUT
 	//NOTE: get rid of all the printing for profiling
 	float maxError = 0.0f;
 	for (int i = 0; i < totalElements; i++) {
-		cout << "CPU: " << i << h_output_cpu[i] << '\n';
+		//cout << "CPU element at " << i << ": " << h_output_cpu[i] << '\n';
 	}
 	cout << "\nCPU Runtime: " << cpuTime.count() << " ms\n\n";
 	for (int i = 0; i < totalElements; i++) {
-		cout << "GPU Naive: " << i << h_output_naive[i] << '\n';
+		//cout << "GPU element at " << i << ": " << h_output_naive[i] << '\n';
 		maxError = fmax(maxError, fabs(h_output_cpu[i] - h_output_naive[i]));
 	}
-	cout << "\nMax Error (Naive): " << maxError << '\n\n';
+	cout << "\nMax Error (Naive): " << maxError << "\n\n";
 	maxError = 0.0f;
 	for (int i = 0; i < totalElements; i++) {
-		cout << "GPU Block & Shared: " << i << h_output_block[i] << '\n';
+		//cout << "GPU Block & Shared at " << i << ": " << h_output_block[i] << '\n';
 		maxError = fmax(maxError, fabs(h_output_cpu[i] - h_output_block[i]));
 	}
 	cout << "\nMax Error (Block): " << maxError;
-
+	*/
+	
 
 	//free the device variables from DRAM
 	cudaFree(d_input);
 	cudaFree(d_output_naive);
 	cudaFree(d_output_block);
+	cudaFree(d_output_warp);
 
 	return 0;
 }
